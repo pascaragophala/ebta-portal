4a60f082a7d4522cd1529de1fbb7a8001815c756
Pasca Ragophala <123935582+pascaragophala@users.noreply.github.com>
2025-12-11 18:09:52 +0200

    Update ebta_single_file_app.py

--- ebta_single_file_app.py
+++ ebta_single_file_app.py
@@ -1424,7 +1424,7 @@ def page(title, body_html, extra_head="", extra_js=""):
         <div class="copyright">
             © <span id="year"></span> Early Bird Testimony Academy · All rights reserved.
         </div>
-        <div style="opacity:0.95;">⚡ Powered by <a href="https://pascalmindtech.netlify.app/" target="_blank" style="color:rgba(255,255,255,0.95);text-decoration:underline;font-weight:600;">Pasca Ragophala</a></div>
+        <div style="opacity:0.95;">⚡ Powered by <a href="https://pascalmindtech.netlify.app/" target="_blank" style="color:#000;text-decoration:underline;font-weight:600;">Pasca Ragophala</a></div>
     </footer>{extra_js}
     </body></html>
     """
