4706f11db5997b93c97dd2edcf3c56c3437b5516
Pasca Ragophala <123935582+pascaragophala@users.noreply.github.com>
2026-03-14 11:55:02 +0200

    Add files via upload

--- ebta_single_file_app.py
+++ ebta_single_file_app.py
@@ -12748,7 +12748,7 @@ def admin_tutor_operations():
 
         {where_clause}
 
-        ORDER BY tw.session_date DESC
+        ORDER BY tw.session_date ASC
     """, params)
 
     rows = cur.fetchall()
