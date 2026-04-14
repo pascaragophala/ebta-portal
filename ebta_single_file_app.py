766db02cf0e63784614b11b7864a21c87f4001a2
Pasca Ragophala <123935582+pascaragophala@users.noreply.github.com>
2026-04-14 11:53:12 +0200

    Add files via upload

--- ebta_single_file_app.py
+++ ebta_single_file_app.py
@@ -5290,10 +5290,6 @@ def student_assignments():
         <div class='card'>
             <a class='btn mini secondary' href='/student'>← Back</a>
 
-            <h2 style="margin-top:10px">
-                📝 Assignments for {pretty_month_label(month)}
-            </h2>
-
             {assignments_html}
         </div>
     """)
