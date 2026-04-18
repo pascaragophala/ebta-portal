ea18f3cf1fe2c3478e5f4947ae9424c470e54631
Pasca Ragophala <123935582+pascaragophala@users.noreply.github.com>
2026-04-18 18:59:36 +0200

    Add files via upload

--- ebta_single_file_app.py
+++ ebta_single_file_app.py
@@ -4639,17 +4639,6 @@ def student_home():
             
             can_resubmit = True
 
-            if a['due_date']:
-                due = datetime.date.fromisoformat(a['due_date'][:10])
-                if today > due:
-                    can_resubmit = False
-            print("DEBUG ASSIGNMENT:", a['title'])
-            print("MONTH:", month)
-            print("TODAY:", today)
-            print("DUE:", a['due_date'])
-            print("CAN_RESUBMIT:", can_resubmit)
-            print("----------------------")
-
             if sub:
                 status = f"<span class='chip active'>Submitted</span>"
 
